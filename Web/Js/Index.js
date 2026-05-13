aa9f8a61adfb6249a38719bcb885f541e2f3cfdf
Ramiro0318 <ramiro.val03@gmail.com>
2026-05-13 07:25:44 -0600

    Add project files.

---
+++ Web/Js/Index.js
@@ -0,0 +1,17 @@
+﻿
+const tablero = document.querySelector('#tablaJugador');
+
+tablero.addEventListener('click', function (event) {
+    const celda = event.target;
+
+    if (celda.tagName === 'TD') {
+        if (celda.textContent === "") {
+            celda.textContent = "💥";
+            celda.style.fontSize = "20px";
+            celda.style.textAlign = "center";
+            celda.style.color = "blue";
+        } else {
+            console.log("Esta celda ya fue atacada.");
+        }
+    }
+});
